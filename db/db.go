d144f392db332222a3b38ff10141e748de2922cc
cmkqwerty <56502922+cmkqwerty@users.noreply.github.com>
2023-12-10 11:13:28 +0300

    [fix] global db variables

--- db/db.go
+++ db/db.go
@@ -1,3 +1,7 @@
 package db
 
-const DBNAME = "movie-ticket-booking"
+const (
+	DBNAME     = "movie-ticket-booking"
+	TestDBNAME = "movie-ticket-booking-test"
+	DBURI      = "mongodb://localhost:27017"
+)
